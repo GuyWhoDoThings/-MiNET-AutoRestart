db545683e90842979dd267f458a9e34d0e4b54e2
GuyWhoDoThings <oslavsky.jan@seznam.cz>
2017-07-14 10:48:26 +0200

    Update Restarter.cs

--- Restarter.cs
+++ Restarter.cs
@@ -1,1 +1,26 @@
+using System;
+using System.IO;
+using MiNET.Worlds;
+using MiNET;
 
+namespace Restarter{
+
+   public class Restarter : Plugin{
+   
+   public Timer _timer;
+   
+      private override onEnable(){
+        Console.WriteLine("Enabled Restarted for MiNET");
+        _timer = new Timer(RestartTick, null, 1, 1000);
+      }
+      
+      private int time = 60;
+      
+      public void RestartTick(object state){
+          time--;
+          if(time == 0){
+              
+          }
+      }         
+      }
+}
